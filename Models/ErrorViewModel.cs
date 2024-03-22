e290df40ae3ecbfe407fb3423633b36ce48f4c9d
andyh <andyh@DESKTOP-SSULCE7>
2024-03-22 14:13:58 +0000

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Driving_School_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
